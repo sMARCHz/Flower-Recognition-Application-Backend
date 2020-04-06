56dc4c7b97ef857ef4936af49a2d5ecc2e5b7c12
sMARCHz <mart_nattanon@hotmail.com>
2020-04-07 02:21:46 +0700

    commit

--- config/db_predmodel.js
+++ config/db_predmodel.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 const predictSchema = new Schema({
-    img: { 
-        data: Buffer, contentType: String, uri:  String
-    },
     userid: {
         type: Number,
         trim: true,
         required: true,
     },
+    class: {
+        type: String,
+        trim: true,
+        required: true,
+        lowercase: true
+    }
 })
 const PredictModel = mongoose.model('prediction', predictSchema);
 module.exports = PredictModel;
