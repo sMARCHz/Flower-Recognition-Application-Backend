66b231a2edddf0a432eb4cd386ce44982d3608ef
sMARCHz <mart_nattanon@hotmail.com>
2020-04-07 02:16:55 +0700

    commit

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get('/getpred/:id', async (req,res)=>{
     const id = req.params.id;
     const product = await predModel.findOne({'userid': id});
     console.log('product',product);
-    res.send(product[]);
+    res.send(product['class']);
 });
 
 app.get('/', function(req,res){
