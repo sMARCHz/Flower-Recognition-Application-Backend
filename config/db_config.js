54d0d4c946e2f0f803c22404eadd88a85228391b
sMARCHz <mart_nattanon@hotmail.com>
2020-04-06 20:15:58 +0700

    commit

--- config/db_config.js
+++ config/db_config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //const db = 'mongodb://127.0.0.1:27017/';
 //const db = process.env.MongoDB_URL;
-const db = 'mongodb+srv://server:flowey@flower1-b6hxe.mongodb.net/test?retryWrites=true&w=majority';
+const db = 'mongodb+srv://server:flowey@flower1-b6hxe.mongodb.net/Flowey?retryWrites=true&w=majority';
 const connectDB = async () => {
     try {
       await mongoose.connect(db, {
