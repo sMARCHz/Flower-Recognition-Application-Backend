4f96bc2c20b66aaa346510b7c83a201f1d4de30d
sMARCHz <mart_nattanon@hotmail.com>
2020-04-30 00:09:16 +0700

    blog

--- server.js
+++ server.js
@@ -75,12 +75,7 @@ app.post('/upblog', async (req,res)=>{
 });
 app.get('/getblog', async (req,res)=>{ //get all blog some info
     try{
-        const blog = await BlogModel.find({},{
-            fields: {
-                blogid: 1,
-                title: 1
-            }
-        })
+        const blog = await BlogModel.find({},{fields: {data: 0}});
         const item = {
             blogid: blog['blogid'],
             title: blog['title']
